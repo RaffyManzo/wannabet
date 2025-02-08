103becfd86633cb68e9a7aaf081ff957d7bea7fd
RaffyManzo <raffaelemanzo1995@gmail.com>
2025-02-08 11:39:12 +0100

    first commit

---
+++ wannabet/src/main/java/is/project/wannabet/WannabetApplication.java
@@ -0,0 +1,13 @@
+package is.project.wannabet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WannabetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WannabetApplication.class, args);
+	}
+
+}
---
+++ wannabet/src/test/java/is/project/wannabet/WannabetApplicationTests.java
@@ -0,0 +1,13 @@
+package is.project.wannabet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WannabetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
