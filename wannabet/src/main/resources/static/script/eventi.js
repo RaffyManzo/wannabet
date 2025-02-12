faa438c6182216b27168a21d8fdb238f2cf730f1
Sestimo <106800749+Sestimo117@users.noreply.github.com>
2025-02-12 18:50:21 +0100

    Fix sintassi

--- wannabet/src/main/resources/static/script/eventi.js
+++ wannabet/src/main/resources/static/script/eventi.js
@@ -1,15 +1,10 @@
 let categorie = ["Champions league", "Europa League", "Serie A", "Serie B", "Premier League"];
 let listCategorie = document.getElementById("categorie");
-for (let i = 0; i < categorie.length; i++){
-    let innerButton = document.createElement("button");
-    innerButton.textContent = categorie[i];
-    innerButton.style.gridColumn = (i%4)+1;
-    innerButton.setAttribute("onclick", "ricercaEventi()");
-    innerButton.setAttribute("class", "buttonCategory");
-    listCategorie.appendChild(innerButton);
-}
-
 let eventi = [];
+let listEventi = document.getElementById("partiteRecenti");
+
+setCategorie();
+
 for ( let i  = 0; i < 6; i++){
     let d = new Date();
     d.setFullYear(2020);
@@ -25,27 +20,7 @@ for ( let i  = 0; i < 6; i++){
         };
 }
 
-eventi.sort((a, b) =>{
-    let x = new Date(a.data);
-    let y = new Date(b.data);
-    if(x.getTime() < y.getTime()){
-        return -1;
-    }else if(x.getTime() > y.getTime()){
-        return 1;
-    }
-    return 0;
-});
-
-let listEventi = document.getElementById("partiteRecenti");
-for(let i = 0; i < 6; i++){
-    let innerButton = document.createElement("button");
-    console.log(eventi[i].name);
-    innerButton.innerHTML = eventi[1].descrizione+": "+ eventi[i].nome;
-    innerButton.style.gridColumn = (i%2)+1;
-    innerButton.setAttribute("class", "buttonEvent");
-    listEventi.appendChild(innerButton);
-
-}
+setEventi();
 
 function ricercaEventi(){
     fetch("api/evento")
@@ -53,5 +28,37 @@ function ricercaEventi(){
         .then(data => {
             console.log(data);
         })
-        .catch(error => console.error("errore", error));
+        .catch(error => console.error("Errore", error));
+}
+
+function setCategorie(){
+    for (let i = 0; i < categorie.length; i++){
+        let innerButton = document.createElement("button");
+        innerButton.textContent = categorie[i];
+        innerButton.style.gridColumn = (i%4)+1;
+        innerButton.setAttribute("onclick", "ricercaEventi()");
+        innerButton.setAttribute("class", "buttonCategory");
+        listCategorie.appendChild(innerButton);
+    }
+}
+
+function setEventi(){
+    for(let i = 0; i < eventi.length; i++){
+        let innerButton = document.createElement("button");
+        console.log(eventi[i].name);
+        innerButton.innerHTML = eventi[i].descrizione+": "+ eventi[i].nome;
+        innerButton.style.gridColumn = (i%2)+1;
+        innerButton.setAttribute("class", "buttonEvent");
+        listEventi.appendChild(innerButton);
+    }
+    eventi.sort((a, b) =>{
+        let x = new Date(a.data);
+        let y = new Date(b.data);
+        if(x.getTime() < y.getTime()){
+            return -1;
+        }else if(x.getTime() > y.getTime()){
+            return 1;
+        }
+        return 0;
+    });
 }
