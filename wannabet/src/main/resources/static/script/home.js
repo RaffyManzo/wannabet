4d2b42bde36a469c3effb9cc96cac18fa7c60d31
leona <goblintool@gmail.com>
2025-02-19 17:52:38 +0100

    Modifiche generali ad home per Spring, aggiunta area utente e css area utente (ancora da finalizzare)

--- wannabet/src/main/resources/static/script/home.js
+++ wannabet/src/main/resources/static/script/home.js
@@ -1,38 +1,35 @@
-let logged = false;
-let header = document.getElementById('header');
-if(logged){
-    header.innerHTML = '' +
-        '<a href="#" class ="logo">Wannabet</a>' +
-        '<nav>' +
-        '<ul class ="menu">' +
-        '<li><a href = "#">Sports</a>' +
-        '<ul class ="SubSports">' +
-        '</ul>' +
-        '</li>' +
-        '<li><a href="#">Esports</a>' +
-        '<ul class="SubEsports">' +
-        '</li>' +
-        '</ul>' +
-        '</ul>' +
-        '</nav>' +
-        '<a href="login.html"><button>Login</button></a>' +
-        '<a href = "singup.html"><button>Registrati</button></ahref>'
+let header2 = document.getElementById('header2');
+let header1 = document.getElementById('header1');
 
-}else{
-    header.innerHTML = '' +
-        '<a href="#" class ="logo">Wannabet</a>' +
-        '<nav>' +
-        '<ul class ="menu">' +
-        '<li><a href = "#">Sports</a>' +
-        '<ul class ="SubSports">' +
-        '</ul>' +
-        '</li>' +
-        '<li><a href="#">Esports</a>' +
-        '<ul class="SubEsports">' +
-        '</li>' +
-        '</ul>' +
-        '</ul>' +
-        '</nav>' +
-        '<a href = "#"><button>Area Personale</button></a>'
+header1.innerHTML = '' +
+    '<a href="#" class ="logo">Wannabet</a>' +
+    '<nav>' +
+    '<ul class ="menu">' +
+    '<li><a href = "#">Sports</a>' +
+    '<ul class ="SubSports">' +
+    '</ul>' +
+    '</li>' +
+    '<li><a href="#">Esports</a>' +
+    '<ul class="SubEsports">' +
+    '</li>' +
+    '</ul>' +
+    '</ul>' +
+    '</nav>' +
+    '<a href="login.html"><button>Login</button></a>' +
+    '<a href = "singup.html"><button>Registrati</button></a>';
 
-}
+header2.innerHTML = '' +
+    '<a href="#" class ="logo">Wannabet</a>' +
+    '<nav>' +
+    '<ul class ="menu">' +
+    '<li><a href = "#">Sports</a>' +
+    '<ul class ="SubSports">' +
+    '</ul>' +
+    '</li>' +
+    '<li><a href="#">Esports</a>' +
+    '<ul class="SubEsports">' +
+    '</li>' +
+    '</ul>' +
+    '</ul>' +
+    '</nav>' +
+    '<a href = "#"><button>Area Personale</button></a>';
